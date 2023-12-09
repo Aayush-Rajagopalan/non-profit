66a495eac836bf3512eb48d34adf7372dc9afaaf
Aayush Rajagopalan <61064137+Aayush-Rajagopalan@users.noreply.github.com>
2023-12-09 11:18:08 +0300

    Update color styles in components

--- components/title.tsx
+++ components/title.tsx
@@ -6,7 +6,7 @@ const SectionTitle = (props:any) => {
         props.align === "left" ? "" : "items-center justify-center text-center"
       }`}>
       {props.pretitle && (
-        <div className="text-sm font-bold tracking-wider text-indigo-600 uppercase">
+        <div className="text-sm font-bold tracking-wider text-blue-600 uppercase">
           {props.pretitle}
         </div>
       )}
